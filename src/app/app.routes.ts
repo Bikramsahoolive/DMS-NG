fbdf530b022277bc3db9b741ebba2decde861019
Bikram Sahoo <bikramsahoo@live.in>
2025-01-28 18:59:35 +0530

    build user dashboard and improve ui.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { DashboardComponent } from './components/after-login/dashboard/dashboard.component';
 
 export const routes: Routes = [
     {path:'',component:LoginComponent},
     {path:'forgot-password',component:ForgotPasswordComponent},
-    // { path: '**', redirectTo: '' }
+    {path:'dashboard',component:DashboardComponent},
+    { path: '**', redirectTo: '' }
 ];
